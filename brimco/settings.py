cccb99fe51eae7ad9c879ee53845166128e19e0a
Bri McOmber <bri.mcomber@gmail.com>
2020-09-10 17:53:55 -0700

    remove project_root

--- brimco/settings.py
+++ brimco/settings.py
@@ -129,8 +129,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 # STATIC_URL = '/static/'
