a42f5ab52f61c45621fabba149518b74fa7d32e0
Bri McOmber <bri.mcomber@gmail.com>
2020-09-10 17:52:22 -0700

    update settings

--- brimco/settings.py
+++ brimco/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 
 ## https://simpleisbetterthancomplex.com/tutorial/2016/08/09/how-to-deploy-django-applications-on-heroku.html
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
