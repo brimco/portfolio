432b15c436015de78919cbe639c8564ec2fdb1f0
Bri McOmber <bri.mcomber@gmail.com>
2020-09-18 13:39:13 -0700

    initial layout

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 from . import views  
 
 app_name = "portfolio"
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path('projects', views.projects, name='projects'),
+    path('contact', views.contact, name='contact')
+]
--- portfolio/views.py
+++ portfolio/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 def index(request):
     # return HttpResponse('<h1>Home</h1>')
     return render(request, 'index.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def contact(request):
+    return render(request, 'contact.html')
