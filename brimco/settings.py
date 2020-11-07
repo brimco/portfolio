fdcccb7320e0cb732f6c92e4b7c00d68602cb725
Bri McOmber <bri.mcomber@gmail.com>
2020-11-07 15:44:59 -0700

    Set debug to false

--- brimco/settings.py
+++ brimco/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['brimco.herokuapp.com', '127.0.0.1', '192.168.1.6']
 
